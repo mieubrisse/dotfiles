c7cc4431d8a404b5022edc84fc231d51506a77cf
Kevin Today <ktoday@palantir.com>
2017-11-19 16:53:42 +0400

    Remove config.json and init.py in favor of init.sh

---
+++ init.sh
@@ -0,0 +1,38 @@
+# Little script to initialize all my dotfiles and things on a new box
+
+
+
+
+set -euo pipefail
+script_dirpath="$(cd "$(dirname "${0}")" && pwd)"
+
+# Big array of symlinks to create in the form (source,link to create)
+symlink_arr=(
+	"${script_dirpath}/bash/bash_aliases,${HOME}/.bash_aliases"
+	"${script_dirpath}/bash/bash_logout,${HOME}/.bash_logout"
+	"${script_dirpath}/bash/bashrc,${HOME}/.bashrc"
+	"${script_dirpath}/git/gitignore,${HOME}/.gitignore"
+	"${script_dirpath}/git/gitconfig,${HOME}/.gitconfig"
+	"${script_dirpath}/vim,${HOME}/.vim"
+	"${script_dirpath}/bash/utils,${HOME}/.bash_utils"
+	"${script_dirpath}/bash/editrc,${HOME}/.editrc"
+	"${script_dirpath}/bash/bash_completion,${HOME}/.bash_completion"
+	"${script_dirpath}/bash/bash-completions,${HOME}/.bash-completions"
+	"${script_dirpath}/tmux/tmux.conf,${HOME}/.tmux.conf"
+	"${HOME}/.bashrc,${HOME}/.bash_profile"
+	"${HOME}/vim/vimrc,${HOME}/.vimrc"
+)
+
+for link_def in "${symlink_arr[@]}"; do
+	source_filepath="$(echo "${link_def}" | cut -d',' -f1)"
+	link_filepath="$(echo "${link_def}" | cut -d',' -f2)"
+	ln -sfn "${source_filepath}" "${link_filepath}"
+done
+
+# Set up iPython niceties
+ipython_dirpath="${HOME}/.ipython/profile_default"
+mkdir -p "${ipython_dirpath}"
+ln -s "${script_dirpath}/ipython/ipython_config.py" "${ipython_dirpath}/ipython_config.py"
+
+# Save less keybindings
+lesskey "${script_dirpath}/less/lesskey"
